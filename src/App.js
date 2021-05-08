44732cf8b53778bca65122613a56ff3e0382c799
Jose Negrin <joseantonionegrin@gmail.com>
2021-05-08 12:34:01 +0200

    portfolio de lisa
    Portfolio de lisa creado con react y usando styled components.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainSection from './components/MainSection';
+import MainTwo from './components/MainTwo';
+import Proyectos from './components/Proyectos';
+import Contact from './components/Contact';
+import BackgroundForms from './components/BackgroundForms';
+import styled from '@emotion/styled';
+
+const BodyContainer = styled.div`
+  background: linear-gradient(135deg, #FAD7A1 0%, #E96D71 100%);
+  margin: 0;
+  padding: 0;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BodyContainer>
+      <BackgroundForms/>
+      <Header/>
+      <MainSection/>
+      <MainTwo/>
+      <Proyectos/>
+      <Contact/>
+      <Footer/>
+    </BodyContainer>
   );
 }
 
